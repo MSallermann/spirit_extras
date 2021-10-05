6ad4ff9ac6b828ece06bb7d39a19a6ba96e0d3cd
Moritz Sallermann <m.sallermann@fz-juelich.de>
2021-10-05 17:29:30 +0000

    Added set_output_folder in util and removed some files that are not needed (yet)

--- spirit_utils/util.py
+++ spirit_utils/util.py
@@ -1,5 +1,27 @@
-def set_output_folder():
-    pass
+def set_output_folder(p_state, folder, tag="<time>"):
+    from spirit.parameters import ema, gneb, llg, mc, mmf
+    from spirit import log
+    import os
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    log.set_output_folder(p_state, folder)
+    log.set_output_file_tag(p_state, tag)
+
+    # ema missing
+
+    llg.set_output_folder(p_state, folder)
+    llg.set_output_tag(p_state, tag)
+
+    gneb.set_output_folder(p_state, folder)
+    gneb.set_output_tag(p_state, tag)
+
+    mc.set_output_folder(p_state, folder)
+    mc.set_output_tag(p_state, tag)
+
+    mmf.set_output_folder(p_state, folder)
+    mmf.set_output_tag(p_state, tag)
 
 def find_saddle_point_only():
     pass
