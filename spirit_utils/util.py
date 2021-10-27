024030e8d9fc855e927911850c25e13aab902f2b
Moritz Sallermann <m.sallermann@fz-juelich.de>
2021-10-27 16:42:56 +0000

    util: added estimate_mc_runtime

--- spirit_utils/util.py
+++ spirit_utils/util.py
@@ -23,6 +23,28 @@ def set_output_folder(p_state, folder, tag="<time>"):
     mmf.set_output_folder(p_state, folder)
     mmf.set_output_tag(p_state, tag)
 
+
+def estimate_mc_runtime(p_state, total_iterations, n_iterations_test=500):
+    """Estimates the total run time of 'total iterations' monte carlo steps by measuring `n_iterations_test` monte carlo steps"""
+    from spirit import simulation
+    from spirit import parameters
+
+    parameters.mc.set_iterations(p_state, n_iterations_test, n_iterations_test) # We want n_iterations iterations and only a single log message
+    info = simulation.start(p_state, simulation.METHOD_MC) # Start a MC simulation
+
+    # total_iterations = len(sample_temperatures) * (n_samples*n_decorrelation + n_thermalisation)
+    runtime_seconds = total_iterations / info.total_ips
+
+    hours   = int( (runtime_seconds / (60**2)) )
+    minutes = int( (runtime_seconds - hours * 60**2) / 60 )
+    seconds = int( (runtime_seconds - hours * 60**2 - minutes*60) )
+
+    print("Estimated runtime = {:.0f}h:{:.0f}m:{:.0f}s".format( hours, minutes, seconds ) )
+    print("Total iterations  = {:.0f}".format( total_iterations ) )
+    print("IPS               = {:.0f}".format( info.total_ips ) )
+
+    return runtime_seconds
+
 def find_saddle_point_only():
     pass
 
