58f3063bdaef7c63610ce63c35ebfe0b41569052
Moritz <m.sallermann@fz-juelich.de>
2022-05-18 16:25:27 +0200

    pyvista_plotting: added interpolate_point_cloud and improved minor things in Spin_Plotter

--- src/spirit_extras/pyvista_plotting.py
+++ src/spirit_extras/pyvista_plotting.py
@@ -11,6 +11,39 @@ def create_point_cloud(spin_system):
     point_cloud["spins_rgba"] = get_rgba_colors( spin_system.spins, opacity=1.0 )
     return point_cloud
 
+def interpolate_point_cloud(spin_system, point_cloud=None, factor=1):
+    if point_cloud is None:
+        point_cloud = create_point_cloud(spin_system)
+
+    nos       = spin_system.nos()
+    nos_after = spin_system.nos() * factor**3
+    n_cells   = spin_system.n_cells
+
+    spin_system.shape()
+    offset_a = spin_system.positions[0, n_cells[0]-1, 0, 0] / factor
+    offset_b = spin_system.positions[0, 0, n_cells[1]-1, 0] / factor
+    offset_c = spin_system.positions[0, 0, 0, n_cells[2]-1] / factor
+    spin_system.flatten()
+
+    print(offset_a)
+    print(offset_b)
+    print(offset_c)
+
+    positions_interpolated = np.empty( shape = (nos_after, 3) )
+
+    for a in range(factor):
+        for b in range(factor):
+            for c in range(factor):
+                offset_vector = a * offset_a + b * offset_b + c * offset_c
+
+                idx_1 = (a + factor * (b + factor * c)) * nos
+                positions_interpolated[idx_1 : idx_1 + nos] = spin_system.positions / factor + offset_vector
+
+    print(spin_system.positions)
+    print(positions_interpolated)
+
+    return pv.PolyData( positions_interpolated ).interpolate(point_cloud)
+
 def delaunay(point_cloud):
     return point_cloud.delaunay_3d(progress_bar=True)
 
@@ -21,16 +54,16 @@ def isosurface_from_delaunay(delaunay, isovalue=0, scalars_key="spins_z"):
     isosurface = isosurface.smooth()
     return isosurface
 
-def arrows_from_point_cloud(point_cloud):
+def arrows_from_point_cloud(point_cloud, factor=1):
     import pyvista as pv
     geom   = pv.Cone(radius=0.25, resolution=18)
-    arrows = point_cloud.glyph(orient="spins", scale=False, factor=1, geom=geom)
+    arrows = point_cloud.glyph(orient="spins", scale=False, factor=factor, geom=geom)
     return arrows
 
-def create_pre_image(pre_image_spin, point_cloud, tol=0.05):
+def create_pre_image(pre_image_spin, point_cloud, angle_tolerance=0.05, n_neighbours=10):
     from .post_processing import compute_pre_image
     import pyvista as pv
-    positions = compute_pre_image( point_cloud.points, point_cloud["spins"], pre_image_spin )[1]
+    positions = compute_pre_image( point_cloud.points, point_cloud["spins"], pre_image_spin, angle_tolerance=angle_tolerance, n_neighbours=n_neighbours )[1]
     return pv.Spline(positions).tube(radius=0.3)
 
 def save_to_png(image_file_name, mesh_list):
@@ -69,8 +102,13 @@ import pyvista as pv
 class Spin_Plotter:
 
     def __init__(self, system):
+        self.spin_system  = system
+
         self._point_cloud = create_point_cloud(system)
+
         self._delaunay    = None
+
+        self.background_color = "white"
 
         self.meshlist = []
 
@@ -131,6 +169,10 @@ class Spin_Plotter:
         if self._delaunay:
             self._delaunay.copy_attributes( self._point_cloud )
 
+    def add_mesh(self, mesh, render_args):
+        self.meshlist.append( [mesh, render_args] )
+        return self.meshlist[-1]
+
     def isosurface(self, isovalue, scalars_key, render_args=None):
         if not self._delaunay:
             raise Exception("No delaunay")
@@ -138,26 +180,23 @@ class Spin_Plotter:
         if render_args is None:
             render_args = self.default_render_args.copy()
 
-        temp = [isosurface_from_delaunay(self._delaunay, isovalue, scalars_key), render_args]
-        self.meshlist.append(temp)
+        return self.add_mesh(isosurface_from_delaunay(self._delaunay, isovalue, scalars_key), render_args)
 
     def arrows(self, render_args=None):
 
         if render_args is None:
             render_args = self.default_render_args.copy()
 
-        temp = [arrows_from_point_cloud(self._point_cloud), render_args]
-        self.meshlist.append( temp )
+        return self.add_mesh(arrows_from_point_cloud(self._point_cloud), render_args)
 
-    def add_preimage(self, spin_dir, tol=0.05, render_args=None):
+    def add_preimage(self, spin_dir, tol=0.05, n_neighbours=10, interpolation_factor=1, render_args={"color" : "black"}):
         if not self._delaunay:
             raise Exception("No delaunay")
 
         if render_args is None:
             render_args = self.default_render_args.copy()
 
-        temp = [create_pre_image(spin_dir, self._point_cloud), render_args]
-        self.meshlist.append( temp )
+        return self.add_mesh( create_pre_image(spin_dir, self._point_cloud, angle_tolerance=tol, n_neighbours=n_neighbours), render_args )
 
     def show(self, save_camera_file=None):
         plotter = pv.Plotter(shape=(1,1))
@@ -170,13 +209,11 @@ class Spin_Plotter:
                 print(f"Could not add_mesh {m}")
                 print(e)
 
-        plotter.set_background("white")
+        if not self.background_color is None:
+            plotter.set_background(self.background_color)
+
         plotter.add_axes(color="black", line_width=6)
         plotter.show()
-        print(f"position    = {plotter.camera.position}")
-        print(f"up          = {plotter.camera.up}")
-        print(f"focal_point = {plotter.camera.focal_point}")
-        print(f"distance = {plotter.camera.distance}")
 
         if save_camera_file is not None:
             camera_dict = dict(
@@ -201,6 +238,8 @@ class Spin_Plotter:
                 print(f"Could not add_mesh {m}")
                 print(e)
 
-        plotter.set_background("white")
+        if not self.background_color is None:
+            plotter.set_background(self.background_color)
+
         plotter.add_axes(color="black", line_width=6)
         plotter.show(screenshot= png_path + ".png")
