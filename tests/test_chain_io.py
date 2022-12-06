861b481397c0b19df4a8a13fa04f815e272e35cd
Moritz Sallermann <m.sallermann@fz-juelich.de>
2022-12-06 02:21:27 +0000

    tests: test_chain_io converted to proper unittest

--- tests/test_chain_io.py
+++ tests/test_chain_io.py
@@ -1,150 +1,159 @@
-from . import util
+import unittest
+
+from spirit_extras import chain_io
 import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 INPUT_FILE = os.path.join(SCRIPT_DIR, "inputs/test_chain_io.cfg")
 QUIET = True
 
-from ..spirit_utils import chain_io
 import numpy as np
 from spirit import state, chain, configuration, system, io
 
 
-def test_write_between():
-    OUTPUT_FILE = os.path.join(SCRIPT_DIR, "output_test_chain_io.ovf")
-    with state.State(INPUT_FILE, QUIET) as p_state:
-        configuration.plus_z(p_state)
-        chain.image_to_clipboard(p_state)
+class Chain_IO_Test(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
 
-        noi = 10
-        chain.set_length(p_state, noi)
-        for idx_image in range(noi):
-            configuration.skyrmion(p_state, radius=2 * idx_image, idx_image=idx_image)
+    def tearDown(self) -> None:
+        pass
 
-        # Test write between
-        idx_1 = 2
-        idx_2 = 5
+    def test_write_between(self):
+        OUTPUT_FILE = os.path.join(SCRIPT_DIR, "output_test_chain_io.ovf")
+        with state.State(INPUT_FILE, QUIET) as p_state:
+            configuration.plus_z(p_state)
+            chain.image_to_clipboard(p_state)
 
-        old_spin_directions = []
-        for idx in range(idx_1, idx_2 + 1):
-            old_spin_directions.append(
-                np.array(system.get_spin_directions(p_state, idx_image=idx))
-            )
+            noi = 10
+            chain.set_length(p_state, noi)
+            for idx_image in range(noi):
+                configuration.skyrmion(
+                    p_state, radius=2 * idx_image, idx_image=idx_image
+                )
 
-        chain_io.chain_write_between(p_state, OUTPUT_FILE, idx_1, idx_2)
-        chain.set_length(p_state, 1)
-        io.chain_read(p_state, OUTPUT_FILE)
+            # Test write between
+            idx_1 = 2
+            idx_2 = 5
 
-        for i, idx in enumerate(range(idx_1, idx_2 + 1)):
-            new_spin_directions = np.array(
-                np.array(system.get_spin_directions(p_state, idx_image=i))
-            )
-            print("Testing image {}, original idx {}".format(i, idx))
-            res = np.max(
-                np.ravel(old_spin_directions[i]) - np.ravel(new_spin_directions)
-            )
-            print(res)
-            assert res < 1e-10
-
-
-def test_write_split_at():
-    with state.State(INPUT_FILE, QUIET) as p_state:
-        configuration.plus_z(p_state)
-        chain.image_to_clipboard(p_state)
-
-        noi = 10
-        chain.set_length(p_state, noi)
-        for idx_image in range(noi):
-            configuration.skyrmion(p_state, radius=2 * idx_image, idx_image=idx_image)
-
-        # Test write split_at
-        filenames = [
-            os.path.join(SCRIPT_DIR, n) for n in ["output_1.ovf", "output_2.ovf"]
-        ]
-        idx_list = [2, 3, 9]
-        idx_pairs = [[2, 3], [3, 9]]
-
-        old_spin_directions = []
-        for p in idx_pairs:
-            old_spin_directions.append([])
-            for idx in range(p[0], p[1] + 1):
-                old_spin_directions[-1].append(
+            old_spin_directions = []
+            for idx in range(idx_1, idx_2 + 1):
+                old_spin_directions.append(
                     np.array(system.get_spin_directions(p_state, idx_image=idx))
                 )
 
-        chain_io.chain_write_split_at(
-            p_state, idx_list=idx_list, filename_list=filenames
-        )
-
-        for j, (f, p) in enumerate(zip(filenames, idx_pairs)):
+            chain_io.chain_write_between(p_state, OUTPUT_FILE, idx_1, idx_2)
             chain.set_length(p_state, 1)
-            io.chain_read(p_state, f)
-            idx_1 = p[0]
-            idx_2 = p[1]
+            io.chain_read(p_state, OUTPUT_FILE)
+
             for i, idx in enumerate(range(idx_1, idx_2 + 1)):
                 new_spin_directions = np.array(
                     np.array(system.get_spin_directions(p_state, idx_image=i))
                 )
                 print("Testing image {}, original idx {}".format(i, idx))
                 res = np.max(
-                    np.ravel(old_spin_directions[j][i]) - np.ravel(new_spin_directions)
+                    np.ravel(old_spin_directions[i]) - np.ravel(new_spin_directions)
                 )
-                print(res)
-                assert res < 1e-10
+                self.assertAlmostEqual(res, 1e-10)
 
+    def test_write_split_at(self):
+        with state.State(INPUT_FILE, QUIET) as p_state:
+            configuration.plus_z(p_state)
+            chain.image_to_clipboard(p_state)
 
-def test_append_to_file_from_file():
-    FILE_IN = os.path.join(SCRIPT_DIR, "in.ovf")
-    FILE_OUT = os.path.join(SCRIPT_DIR, "out.ovf")
+            noi = 10
+            chain.set_length(p_state, noi)
+            for idx_image in range(noi):
+                configuration.skyrmion(
+                    p_state, radius=2 * idx_image, idx_image=idx_image
+                )
 
-    if os.path.exists(FILE_OUT):
-        os.remove(FILE_OUT)
+            # Test write split_at
+            filenames = [
+                os.path.join(SCRIPT_DIR, n) for n in ["output_1.ovf", "output_2.ovf"]
+            ]
+            idx_list = [2, 3, 9]
+            idx_pairs = [[2, 3], [3, 9]]
 
-    with state.State(INPUT_FILE, QUIET) as p_state:
-        configuration.plus_z(p_state)
-        chain.image_to_clipboard(p_state)
+            old_spin_directions = []
+            for p in idx_pairs:
+                old_spin_directions.append([])
+                for idx in range(p[0], p[1] + 1):
+                    old_spin_directions[-1].append(
+                        np.array(system.get_spin_directions(p_state, idx_image=idx))
+                    )
 
-        noi = 2
-        chain.set_length(p_state, noi)
-        for idx_image in range(noi):
-            configuration.skyrmion(p_state, radius=2 * idx_image, idx_image=idx_image)
-
-        old_spin_directions = []
-        for idx in range(noi):
-            old_spin_directions.append(
-                np.array(system.get_spin_directions(p_state, idx_image=idx))
+            chain_io.chain_write_split_at(
+                p_state, idx_list=idx_list, filename_list=filenames
             )
 
-        io.chain_write(p_state, FILE_IN)
+            for j, (f, p) in enumerate(zip(filenames, idx_pairs)):
+                chain.set_length(p_state, 1)
+                io.chain_read(p_state, f)
+                idx_1 = p[0]
+                idx_2 = p[1]
+                for i, idx in enumerate(range(idx_1, idx_2 + 1)):
+                    new_spin_directions = np.array(
+                        np.array(system.get_spin_directions(p_state, idx_image=i))
+                    )
+                    print("Testing image {}, original idx {}".format(i, idx))
+                    res = np.max(
+                        np.ravel(old_spin_directions[j][i])
+                        - np.ravel(new_spin_directions)
+                    )
+                    self.assertAlmostEqual(res, 1e-10)
 
-        chain.set_length(p_state, 1)
-        configuration.random(p_state)
+    def test_append_to_file_from_file(self):
+        FILE_IN = os.path.join(SCRIPT_DIR, "in.ovf")
+        FILE_OUT = os.path.join(SCRIPT_DIR, "out.ovf")
 
-        chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
-        chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
-        chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
-        chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
+        if os.path.exists(FILE_OUT):
+            os.remove(FILE_OUT)
 
-        chain.set_length(p_state, 1)
-        io.chain_read(p_state, FILE_OUT)
+        with state.State(INPUT_FILE, QUIET) as p_state:
+            configuration.plus_z(p_state)
+            chain.image_to_clipboard(p_state)
 
-        for i in range(4):
-            print("Testing image {}, original idx {}".format(i, idx))
+            noi = 2
+            chain.set_length(p_state, noi)
+            for idx_image in range(noi):
+                configuration.skyrmion(
+                    p_state, radius=2 * idx_image, idx_image=idx_image
+                )
 
-            new_spin_directions = np.array(
-                np.array(system.get_spin_directions(p_state, idx_image=2 * i))
-            )
-            res = np.max(
-                np.ravel(old_spin_directions[0]) - np.ravel(new_spin_directions)
-            )
-            print(res)
-            assert res < 1e-10
+            old_spin_directions = []
+            for idx in range(noi):
+                old_spin_directions.append(
+                    np.array(system.get_spin_directions(p_state, idx_image=idx))
+                )
 
-            new_spin_directions = np.array(
-                np.array(system.get_spin_directions(p_state, idx_image=2 * i + 1))
-            )
-            res = np.max(
-                np.ravel(old_spin_directions[1]) - np.ravel(new_spin_directions)
-            )
-            print(res)
-            assert res < 1e-10
+            io.chain_write(p_state, FILE_IN)
+
+            chain.set_length(p_state, 1)
+            configuration.random(p_state)
+
+            chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
+            chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
+            chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
+            chain_io.chain_append_to_file_from_file(p_state, FILE_OUT, FILE_IN)
+
+            chain.set_length(p_state, 1)
+            io.chain_read(p_state, FILE_OUT)
+
+            for i in range(4):
+                print("Testing image {}, original idx {}".format(i, idx))
+
+                new_spin_directions = np.array(
+                    np.array(system.get_spin_directions(p_state, idx_image=2 * i))
+                )
+                res = np.max(
+                    np.ravel(old_spin_directions[0]) - np.ravel(new_spin_directions)
+                )
+                self.assertAlmostEqual(res, 1e-10)
+
+                new_spin_directions = np.array(
+                    np.array(system.get_spin_directions(p_state, idx_image=2 * i + 1))
+                )
+                res = np.max(
+                    np.ravel(old_spin_directions[1]) - np.ravel(new_spin_directions)
+                )
+                self.assertAlmostEqual(res, 1e-10)
