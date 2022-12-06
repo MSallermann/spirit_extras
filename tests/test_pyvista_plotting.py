cb829ba03cca003130c390df5a44f7d1e8f9ad92
Moritz Sallermann <m.sallermann@fz-juelich.de>
2022-12-06 19:14:38 +0000

    test_pyvista_plotting: started to rework into proper unittest

--- tests/test_pyvista_plotting.py
+++ tests/test_pyvista_plotting.py
@@ -1,42 +1,66 @@
-import sys, os
+import unittest
+import numpy as np
+from spirit_extras.pyvista_plotting import Spin_Plotter
+from spirit_extras import data
+from spirit import state, configuration
+import os
+
 
 SCRIPT_DIR = os.path.dirname(__file__)
-sys.path.insert(0, os.path.join(SCRIPT_DIR, "../src"))
-
-from spirit_extras import import_spirit, plotting, data, pyvista_plotting
-
-choose = lambda x: x.version_major >= 2 and x.cuda == False
-spirit_libs_list = import_spirit.find_and_insert("~/Coding/spirit", choose=choose)
-
-print(spirit_libs_list[0])
 
 
-def test():
+class Pyvista_Plotting_Test(unittest.TestCase):
+    INPUT_FILE = os.path.join(SCRIPT_DIR, "inputs/test_pyvista_plot.cfg")
+    PLOT_OUTPUTS = os.path.join(SCRIPT_DIR, "pyvista_test")
 
-    INPUT_FILE = os.path.join(SCRIPT_DIR, "inputs/test_plotting.cfg")
+    def setUp(self) -> None:
+        if not os.path.exists(self.PLOT_OUTPUTS):
+            os.makedirs(self.PLOT_OUTPUTS)
 
-    from spirit import state, configuration
+    def tearDown(self) -> None:
+        pass
 
-    with state.State(INPUT_FILE) as p_state:
-        configuration.plus_z(p_state)
-        configuration.hopfion(p_state, 7)
-        # configuration.skyrmion(p_state, 7)
+    def test(self):
+        with state.State(self.INPUT_FILE, quiet=True) as p_state:
+            configuration.plus_z(p_state)
+            configuration.hopfion(p_state, radius=5)
+            spin_system = data.spin_system_from_p_state(p_state).deepcopy()
 
-        system = data.spin_system_from_p_state(p_state)
+        plotter = Spin_Plotter(spin_system)
+        plotter.resolution = (420, 420)  # low res so test is faster
 
-        plotter = pyvista_plotting.Spin_Plotter(system)
-        plotter.camera_position = "yz"
-        plotter.camera_azimuth = 45
-        plotter.camera_elevation = 50
+        plotter.arrows()
 
-        # plotter.compute_delaunay()
-        # plotter.save_delaunay("delaunay.vtk")
-        plotter.load_delaunay("delaunay.vtk")
+        plotter.render_to_png(
+            os.path.join(
+                os.path.join(self.PLOT_OUTPUTS, "cones_with_white_background.png")
+            )
+        )
 
-        # plotter.add_preimage([1,0,0], tol=0.02)
-        plotter.isosurface(0, "spins_z")
-        # plotter.show()
-        plotter.render_to_png("test")
+        plotter.background_color = "transparent"
+        plotter.render_to_png(
+            os.path.join(
+                os.path.join(self.PLOT_OUTPUTS, "cones_with_transparent_background.png")
+            )
+        )
 
+        plotter.background_color = "black"
+        plotter.render_to_png(
+            os.path.join(
+                os.path.join(self.PLOT_OUTPUTS, "cones_with_black_background.png")
+            )
+        )
 
-test()
+        plotter.axes = True
+        plotter.camera_focal_point = spin_system.center()
+        plotter.camera_focus(distance=120, direction="XYZ")
+        plotter.render_to_png(
+            os.path.join(os.path.join(self.PLOT_OUTPUTS, "cones_with_camera_focus.png"))
+        )
+
+        plotter.colormap("rgb")
+        plotter.clear_meshes()
+        plotter.arrows()
+        plotter.render_to_png(
+            os.path.join(os.path.join(self.PLOT_OUTPUTS, "cones_colormap_rgb.png"))
+        )
